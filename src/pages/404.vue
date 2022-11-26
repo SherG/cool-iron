d63aa3e9542ead7efe42ea189e6e81468d9d6c9a
sherrie gossett <sherrie.g5@gmail.com>
2022-11-26 11:47:28 -0500

    Update 404.vue

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -4,6 +4,8 @@
       <h2 class="text-4xl font-bold mb-16">Page Not Found</h2>
       <g-image src="../../static/404.svg" alt="404 page not found"/>
     </div>
+<p>Preparing a digital strategy? <a href="/acquisition-behavior-outcomes">Start here</a>.</p>
+<p>Or check out <a href="/partnerships">our tips</a> on how to grow a service business with zero budget.
 
   </Layout>
 </template>
