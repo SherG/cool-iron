e513a8e866488be895440caf4343eff53e29b0eb
sherrie gossett <sherrie.g5@gmail.com>
2022-11-26 12:44:35 -0500

    Update 404.vue

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -5,8 +5,8 @@
       <g-image src="../../static/404.svg" alt="404 page not found"/>
    
 <br/><h3><strong>Check out these articles:</strong></h3>
-<p>Preparing a digital strategy? <a href="/acquisition-behavior-outcomes">Start here</a>.</p>
-<p>Or <a href="/partnerships">find out</a> how to grow a service business with zero budget.
+<ul><li>Preparing a digital strategy? <a href="/acquisition-behavior-outcomes">Start here</a>.</li>
+<li>Or <a href="/partnerships">find out how to grow a service business with zero budget</a>.</li></ul>
  </div>
   </Layout>
 </template>
