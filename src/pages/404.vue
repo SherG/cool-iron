9ebbaa75266d420bc940b4a98bed2d40ff4c7f3d
sherrie gossett <sherrie.g5@gmail.com>
2022-11-26 11:48:33 -0500

    Update 404.vue

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <div class="container-inner mx-auto py-16">
-      <h2 class="text-4xl font-bold mb-16">Page Not Found</h2>
+      <h2 class="text-4xl font-bold mb-16">Oops! Page Not Found</h2>
       <g-image src="../../static/404.svg" alt="404 page not found"/>
     </div>
 <p>Preparing a digital strategy? <a href="/acquisition-behavior-outcomes">Start here</a>.</p>
